7f0b83fab31b7bd3a13907b6deef54a59af3b319
R3messa1 <51412906+R3messa1@users.noreply.github.com>
2024-03-25 13:41:04 +0200

    Add files via upload

---
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float currentSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * -1 * currentSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f;
+
+    public Transform gunTransform; 
+
+    private Rigidbody2D body;
+    private Master controls;
+    private Vector2 moveInput;
+
+    private Vector2 aimInput;
+
+
+    private void Awake(){
+
+        controls = new Master();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable(){
+        controls.Enable();
+    }
+
+    private void OnDisable(){
+        controls.Disable();
+    }
+
+    private void FixedUpdate(){
+        Move();
+    }
+
+    private void Move(){
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.fixedDeltaTime; 
+        body.MovePosition(body.position + movement);  }
+
+    private void Update(){
+
+        Shoot();
+
+        if(UsingMouse()){
+            AimWithMouse();
+        }
+        else{
+            AimWithControllerOrKeyBoard();
+        }
+    }
+
+    private void Shoot(){
+
+        if (controls.Player.Shoot.triggered){
+            
+            GameObject bullet = BulletPoolManager.Instance.GetBullet();
+            bullet.transform.position = gunTransform.position;
+            bullet.transform.rotation = gunTransform.rotation;
+        }
+    }
+
+    private bool UsingMouse(){
+
+        if (Mouse.current.delta.ReadValue().sqrMagnitude > 0.1){
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private void AimWithMouse(){
+        
+        aimInput = controls.Player.Aim.ReadValue<Vector2>();
+
+        if(aimInput.sqrMagnitude > 0.1){
+
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            mouseWorldPosition.z = 0;
+
+            Vector2 aimDirection = (mouseWorldPosition - gunTransform.position).normalized;
+
+            aimDirection.y = -aimDirection.y;
+
+            float angle = (Mathf.Atan2(aimDirection.x, aimDirection.y)) * Mathf.Rad2Deg;
+
+            gunTransform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    private void AimWithControllerOrKeyBoard(){
+
+        Vector2 controllerAim = controls.Player.Aim.ReadValue<Vector2>();
+
+        if(controllerAim.sqrMagnitude > 0.1){
+
+            float angle = Mathf.Atan2(controllerAim.x, -contollerAim.y) * Mathf.Rad2Deg;
+            gunTransform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
